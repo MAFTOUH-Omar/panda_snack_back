5ff108baef21358d4753ed13a561da14a9321088
MAFTOUH <omar.maftou0000@gmail.com>
2023-10-08 07:59:12 +0100

    server side

---
+++ app.js
@@ -0,0 +1,33 @@
+const express = require('express')
+const app = express();
+const UserRoute = require('./routes/user.routes') 
+const CategoryRoute = require('./routes/category.routes') 
+const MealRoute = require('./routes/meal.routes') 
+const OrderRoute = require('./routes/order.routes') 
+const db = require("./config/database")
+const fs = require('fs');
+const path = require('path');
+const cors = require('cors');
+
+app.use(cors());
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+//user routes
+app.use('/users',UserRoute);
+//category routes
+app.use('/categorys',CategoryRoute);
+//meal routes
+app.use('/meals',MealRoute);
+//order routes
+app.use('/orders',OrderRoute);
+//main
+app.get('/' , (req,res)=>{
+    res.send('All Oki')
+})
+
+app.use('/meal_picture/', express.static('Picture/meal_picture'));
+
+db.connect();
+app.listen(process.env.APP_PORT);
---
+++ middlewares/verifyToken.middlewares.js
@@ -0,0 +1,42 @@
+const jwt = require("jsonwebtoken");
+const config = process.env;
+
+const verifyToken = (roles) => async (req, res, next) => {
+  const token =
+    req.headers &&
+    req.headers.authorization &&
+    req.headers.authorization.split(" ")[0] === "Bearer";
+
+  if (!token) {
+    return res.status(403).send("A token is required for authentication");
+  }
+
+  try {
+    const decoded = jwt.verify(
+      req.headers.authorization.split(" ")[1],
+      config.TOKEN_KEY
+    );
+
+    // Vérifier si l'utilisateur a le rôle requis
+    if (roles && roles.length > 0 && !roles.includes(decoded.role)) {
+      return res.status(403).send("Unauthorized role");
+    }
+
+    req.user = decoded;
+
+    // Redirection en fonction du rôle
+    if (decoded.role === "admin") {
+        return res.redirect("/admin/dashboard");
+    } else if (decoded.role === "user") {
+        return res.redirect("/user/dashboard");
+    } else if (decoded.role === "delivery") {
+        return res.redirect("/delivery/dashboard");
+    }
+
+    return next();
+  } catch (err) {
+    return res.status(401).send("Invalid Token");
+  }
+};
+
+module.exports = verifyToken;
